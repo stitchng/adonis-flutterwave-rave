3861794415d37feae118da156b1bcb1d133fbd87
StitchNG <stitchnig@gmail.com>
2019-02-12 11:21:00 +0100

    Update FlutterwaveRaveProvider.js

--- providers/FlutterwaveRaveProvider.js
+++ providers/FlutterwaveRaveProvider.js
@@ -1,15 +1,15 @@
-use strict'
+'use strict'
 
 const { ServiceProvider } = require('@adonisjs/fold')
 
 class FlutterwaveRaveProvider extends ServiceProvider {
-  register () {
-    this.app.singleton('Adonis/Addons/FlutterwaveRave', () => {
-      const Config = this.app.use('Adonis/Src/Config')
-      const PayStack = require('../src/FlutterwaveRave')
-      return (new FlutterwaveRave(require('ravepay'), Config)).agent
-    })
-  }
+      register () {
+            this.app.singleton('Adonis/Addons/FlutterwaveRave', () => {
+              const Config = this.app.use('Adonis/Src/Config')
+              const FlutterwaveRave = require('../src/FlutterwaveRave')
+              return (new FlutterwaveRave(require('ravepay'), Config)).agent
+            })
+      }
 }
 
 module.exports = FlutterwaveRaveProvider
