b5dbb835160365cc7f5d19748bf7770f666faf3c
StitchNG <stitchnig@gmail.com>
2019-03-04 10:46:21 +0100

    Update and rename instruction.js to instructions.js

--- instructions.js
+++ instructions.js
@@ -11,8 +11,8 @@
 const path = require('path')
 
 module.exports = async function (cli) {
-  await cli.makeConfig('bugsnag.js', path.join(__dirname, 'config/bugsnag.js'))
+  await cli.makeConfig('ravepay.js', path.join(__dirname, 'config/ravepay.js'))
     .catch((e) => {})
 
-  cli.command.completed('create', 'config/bugsnag.js')
+  cli.command.completed('create', 'config/ravepay.js')
 }
