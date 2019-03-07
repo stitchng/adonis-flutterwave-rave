eeb368ad67d6a7ad5bd240045a6fed5d0779c31c
StitchNG <stitchnig@gmail.com>
2019-03-07 17:39:31 +0100

    Update index.js

--- src/FlutterwaveRave/index.js
+++ src/FlutterwaveRave/index.js
@@ -6,8 +6,8 @@ const Env = use('Env')
 class FlutterwaveRaveAPIClient {
       constructor(Agent, Config) {
           this.agent = new Agent(
-              Config.get('rave.publicKey'),
-              Config.get('rave.privateKey')
+              Config.get('ravepay.publicKey'),
+              Config.get('ravepay.privateKey')
               (Env.get('NODE_ENV') !== 'development') // production flag
           );
         
