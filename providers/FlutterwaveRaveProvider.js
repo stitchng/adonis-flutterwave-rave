0adb802c80d1f423abbda63218f372fbed23bf8b
StitchNG <stitchnig@gmail.com>
2019-03-08 08:16:54 +0100

    Update FlutterwaveRaveProvider.js

--- providers/FlutterwaveRaveProvider.js
+++ providers/FlutterwaveRaveProvider.js
@@ -7,9 +7,11 @@ class FlutterwaveRaveProvider extends ServiceProvider {
     this.app.singleton('Adonis/Addons/FlutterwaveRave', () => {
       const Config = this.app.use('Adonis/Src/Config')
       const Env = this.app.use('Env')
-      const FlutterwaveRave = require('../src/FlutterwaveRave')
+      const FlutterwaveRave = require('../src/FlutterwaveRave/index.js')
       return (new FlutterwaveRave(require('ravepay'), Config, Env)).agent
     })
+    
+    this.app.alias('Adonis/Addons/FlutterwaveRave', 'FlutterwaveRave')
   }
 }
 
